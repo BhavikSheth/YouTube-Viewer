9c82e951baf5e1a42c43ae377abf19a9255bd73a
Bhavik Sheth <bhaviksheth20@gmail.com>
2017-11-17 17:39:15 +0000

    add lodash debounce to throttle search term input and add title

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import _ from 'lodash';
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
@@ -16,7 +17,7 @@ class App extends Component {
       selectedVideo: null
 		}
 		
-	  this.videoSearch('surfboards');
+	  this.videoSearch('react js');
 	}
 	
 	videoSearch(term) {
@@ -29,9 +30,11 @@ class App extends Component {
 	}
 
 	render() {
+		const videoSearch = _.debounce((term) => { this.videoSearch(term) }, 300);
+
 		return (
 			<div>
-				<SearchBar onSearchTermChange={term => this.videoSearch(term)}/>
+				<SearchBar onSearchTermChange={ videoSearch }/>
 				<VideoDetail video={this.state.selectedVideo}/>
 				<VideoList 
 					onVideoSelect={selectedVideo => this.setState({selectedVideo})}
