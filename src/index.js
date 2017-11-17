d05043f9dbfbdf3a485172c63298352bdec6f69c
Bhavik Sheth <bhaviksheth20@gmail.com>
2017-11-17 16:16:55 +0000

    add feature to display selected video

--- src/index.js
+++ src/index.js
@@ -20,15 +20,23 @@ class App extends Component {
 	}
 	
 	videoSearch(term) {
-		YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
-			this.setState({ videos });
+		YTSearch({key: API_KEY, term}, (videos) => {
+			this.setState({
+				videos: videos,
+				selectedVideo: videos[0]
+			});
 		});
 	}
 
 	render() {
 		return (
 			<div>
-				<VideoList videos={this.state.videos}/>
+				<SearchBar onSearchTermChange={term => this.videoSearch(term)}/>
+				<VideoDetail video={this.state.selectedVideo}/>
+				<VideoList 
+					onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+					videos={this.state.videos}  
+				/>
 			</div>
 		);
 	}
